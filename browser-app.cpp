8641d76067c4a184bc83ee24e7ac78826bd3837e
Zachary Lund <zachary.lund@streamlabs.com>
2018-07-17 15:32:21 -0700

    SQUASH ME

--- browser-app.cpp
+++ browser-app.cpp
@@ -55,7 +55,7 @@ void BrowserApp::OnBeforeCommandLineProcessing(
 
 	command_line->AppendSwitch("enable-begin-frame-scheduling");
 	command_line->AppendSwitch("enable-system-flash");
-	command_line->AppendSwitchWithValue("autoplay-policy", "no-user-gesture-required");
+	command_line->AppendArgument("--autoplay-policy=no-user-gesture-required");
 }
 
 void BrowserApp::OnContextCreated(CefRefPtr<CefBrowser>,
